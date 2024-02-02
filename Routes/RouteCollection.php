72152bf25112307c98cf89a2a616bab6acdec92a
Robson Duarte dos Santos <robsduar@gmail.com>
2024-02-02 14:30:05 -0300

    navbar added

--- Routes/RouteCollection.php
+++ Routes/RouteCollection.php
@@ -1,51 +1,10 @@
 <?php
 
 use Source\Controller\Home;
-use Source\Controller\Filme;
-use Source\Controller\Serie;
-use Source\Controller\Anime;
-use Source\Controller\Manga;
-use Source\Controller\Login;
 
 $MyRouter->get('/',[
     function()
     {  
         return  Home::getHome();
-    }
-]);
-
-$MyRouter->get('/filmes',[
-    function()
-    {  
-        return  Filme::getFilme();
-    }
-]);
-
-$MyRouter->get('/series',[
-    function()
-    {  
-        return  Serie::getSerie();
-    }
-]);
-
-$MyRouter->get('/animes',[
-    function()
-    {  
-        return  Anime::getAnime();
-    }
-]);
-
-
-$MyRouter->get('/mangas',[
-    function()
-    {  
-        return  Manga::getManga();
-    }
-]);
-
-$MyRouter->get('/login',[
-    function()
-    {  
-        return  Login::getLogin();
     }
 ]);
